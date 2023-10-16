b6a61e6f647a2c4a6b47cf195a6f01489988adf7
Jurome <juromefernando@gmail.com>
2023-10-16 20:32:06 +0800

    malupet ako

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,18 +1,28 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-const {
-  registerUser,
-  loginUser,
-  logout,
-  forgotPassword,
-  resetPassword,
-} = require("../controllers/authController");
-router.post("/register", registerUser);
-router.post("/login", loginUser);
-router.get("/logout", logout);
+const { registerUser, loginUser, logout, forgotPassword,
+    resetPassword,
+    getUserProfile,
+    updatePassword,
+    updateProfile,
+    allUsers,
+    getUserDetails
 
-router.post("/password/forgot", forgotPassword);
-router.put("/password/reset/:token", resetPassword);
+
+} = require('../controllers/authController');
+const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
+
+router.post('/register', registerUser);
+router.post('/login', loginUser);
+router.get('/logout', logout);
+
+router.post('/password/forgot', forgotPassword);
+router.put('/password/reset/:token', resetPassword);
+router.get('/me', isAuthenticatedUser, getUserProfile)
+router.put('/password/update', isAuthenticatedUser, updatePassword)
+router.put('/me/update', isAuthenticatedUser, updateProfile)
+router.get('/admin/users', isAuthenticatedUser, allUsers)
+router.get('/admin/user/:id', isAuthenticatedUser, getUserDetails)
 
 module.exports = router;
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,19 +1,13 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-const {
-  getProducts,
-  newProduct,
-  getSingleProduct,
-  updateProduct,
-  deleteProduct,
-} = require("../controllers/productController");
-const { isAuthenticatedUser, authorizeRoles } = require("../middlewares/auth");
+const {newProduct, getProducts, getSingleProduct, updateProduct, deleteProduct} = require('../controllers/productController');
+const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
+router.get('/products',isAuthenticatedUser, getProducts)
 
-router.get("/product", isAuthenticatedUser, getProducts);
-// router.post("/product/new", isAuthenticatedUser,authorizeRoles('admin'), newProduct);
-router.post("/product/new", isAuthenticatedUser, newProduct);
-router.get("/product/:id", getSingleProduct);
-router.route("/admin/product/:id", isAuthenticatedUser, authorizeRoles('admin')).put(updateProduct).delete(deleteProduct);
+
+router.post('/product/new', isAuthenticatedUser, newProduct)
+router.get('/product/:id', getSingleProduct);
+router.route('/admin/product/:id', isAuthenticatedUser, authorizeRoles('admin',)).put(updateProduct).delete(deleteProduct);
 
 module.exports = router;
