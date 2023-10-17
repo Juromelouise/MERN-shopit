971ab25d7bd002085d1b1d48630f923dca84ad7b
Jurome <juromefernando@gmail.com>
2023-10-17 09:12:18 +0800

    abay di ko na din nasusundan si sir, kaya basahin niyo nalang codes ko

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -30,11 +30,13 @@ exports.getProducts = async (req,res,next) => {
 	// const products = await Product.find();
 	apiFeatures.pagination(resPerPage);
 	const products = await apiFeatures.query;
+	let filteredProductsCount = products.length;
 	res.status(200).json({
 		success: true,
-		count: products.length,
+		filteredProductsCount,
 		productsCount,
-		products
+		products,
+		resPerPage,
 	})
 }
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from './Components/Layout/Header'
+import Footer from './Components/Layout/Footer'
+import Home from './Components/Home'
+import ProductDetails from './Components/Product/ProductDetails'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} exact="true" />
+          <Route path="/product/:id" element={<ProductDetails />} exact="true" />
+          <Route path="/search/:keyword" element={<Home />} exact="true" />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
