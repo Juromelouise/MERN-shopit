ff6338526d6f412d21408a97ff71be99d970ad00
Jurome <juromefernando@gmail.com>
2023-10-18 19:55:10 +0800

    cloudinary api secret and api key

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,113 +1,112 @@
-const Order = require("../models/order");
-const Product = require("../models/product");
+const Order = require('../models/order');
+const Product = require('../models/product');
 
 exports.newOrder = async (req, res, next) => {
-  const {
-    orderItems,
-    shippingInfo,
-    itemsPrice,
-    taxPrice,
-    shippingPrice,
-    totalPrice,
-    paymentInfo,
-  } = req.body;
+    const {
+        orderItems,
+        shippingInfo,
+        itemsPrice,
+        taxPrice,
+        shippingPrice,
+        totalPrice,
+        paymentInfo
 
-  const order = await Order.create({
-    orderItems,
-    shippingInfo,
-    itemsPrice,
-    taxPrice,
-    shippingPrice,
-    totalPrice,
-    paymentInfo,
-    paidAt: Date.now(),
-    user: req.user._id,
-  });
+    } = req.body;
 
-  res.status(200).json({
-    success: true,
-    order,
-  });
-};
+    const order = await Order.create({
+        orderItems,
+        shippingInfo,
+        itemsPrice,
+        taxPrice,
+        shippingPrice,
+        totalPrice,
+        paymentInfo,
+        paidAt: Date.now(),
+        user: req.user._id
+    })
+
+    res.status(200).json({
+        success: true,
+        order
+    })
+}
 
 exports.getSingleOrder = async (req, res, next) => {
-  const order = await Order.findById(req.params.id).populate(
-    "user",
-    "name email"
-  );
+    const order = await Order.findById(req.params.id).populate('user', 'name email')
 
-  if (!order) {
-    return res.status(404).json({ message: `No Order found with this ID` });
-  }
+    if (!order) {
+        return res.status(404).json({ message: `No Order found with this ID` })
 
-  res.status(200).json({
-    success: true,
-    order,
-  });
-};
+    }
+
+    res.status(200).json({
+        success: true,
+        order
+    })
+}
 
 exports.myOrders = async (req, res, next) => {
-  const orders = await Order.find({ user: req.user.id });
+    const orders = await Order.find({ user: req.user.id })
 
-  res.status(200).json({
-    success: true,
-    orders,
-  });
-};
+    res.status(200).json({
+        success: true,
+        orders
+    })
+}
 
 exports.allOrders = async (req, res, next) => {
-  const orders = await Order.find();
+    const orders = await Order.find()
 
-  let totalAmount = 0;
+    let totalAmount = 0;
 
-  orders.forEach((order) => {
-    totalAmount += order.totalPrice;
-  });
+    orders.forEach(order => {
+        totalAmount += order.totalPrice
+    })
 
-  res.status(200).json({
-    success: true,
-    totalAmount,
-    orders,
-  });
-};
+    res.status(200).json({
+        success: true,
+        totalAmount,
+        orders
+    })
+}
 
 exports.updateOrder = async (req, res, next) => {
-  const order = await Order.findById(req.params.id);
+    const order = await Order.findById(req.params.id)
 
-  if (order.orderStatus === "Delivered") {
-    return res
-      .status(404)
-      .json({ message: `You have already delivered this order` });
-  }
+    if (order.orderStatus === 'Delivered') {
+        return res.status(404).json({ message: `You have already delivered this order` })
 
-  order.orderItems.forEach(async (item) => {
-    await updateStock(item.product, item.quantity);
-  });
+    }
 
-  order.orderStatus = req.body.status;
-  order.deliveredAt = Date.now();
-  await order.save();
+    order.orderItems.forEach(async item => {
+        await updateStock(item.product, item.quantity)
+    })
 
-  res.status(200).json({
-    success: true,
-  });
-};
+    order.orderStatus = req.body.status
+    order.deliveredAt = Date.now()
+    await order.save()
+
+    res.status(200).json({
+        success: true,
+    })
+}
 
 async function updateStock(id, quantity) {
-  const product = await Product.findById(id);
-  product.stock = product.stock - quantity;
-  await product.save({ validateBeforeSave: false });
+    const product = await Product.findById(id);
+    product.stock = product.stock - quantity;
+    await product.save({ validateBeforeSave: false })
 }
 
 exports.deleteOrder = async (req, res, next) => {
-  const order = await Order.findById(req.params.id);
+    const order = await Order.findById(req.params.id)
 
-  if (!order) {
-    return res.status(404).json({ message: `No Order found with this ID` });
-  }
-  await order.remove();
+    if (!order) {
+        return res.status(404).json({ message: `No Order found with this ID` })
+     
+    }
+    await order.remove()
 
-  res.status(200).json({
-    success: true,
-  });
-};
+    res.status(200).json({
+        success: true
+    })
+}
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,9 @@ import Header from './Components/Layout/Header'
 import Footer from './Components/Layout/Footer'
 import Home from './Components/Home'
 import ProductDetails from './Components/Product/ProductDetails'
+import Login from './Components/User/Login'
+import Register from './Components/User/Register';
+
 
 function App() {
   return (
@@ -14,6 +17,9 @@ function App() {
           <Route path="/" element={<Home />} exact="true" />
           <Route path="/product/:id" element={<ProductDetails />} exact="true" />
           <Route path="/search/:keyword" element={<Home />} exact="true" />
+
+          <Route path="/login" element={<Login />} exact="true"/>
+          <Route path="/register" element={<Register />} exact="true" />
         </Routes>
         <Footer />
       </Router>
